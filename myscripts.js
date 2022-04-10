c037b941f10019e3e507a0f37798cd7f2171883b
Joe Kohler <kohlerj1996@gmail.com>
2022-04-09 21:43:59 -0400

    Rock-Paper-Scissors game to understand syntax, problem solving, and to show me whats possible for my main problem to solve a fitness website.

--- myscripts.js
+++ myscripts.js
@@ -1,82 +1,89 @@
-// Setting the scores and selecting our HTML elements.
-let computerScore = 1;
-let playerScore = 1;
+// Selecting elements.
 const pScore = document.getElementById('playerScore');
 const cScore = document.getElementById('computerScore');
 const buttons = document.querySelectorAll('.selection button');
 const showIcon = document.querySelector('.show i');
 const computerShowIcon = document.querySelector('.computer i');
-// The randomClass array below this contains the rock,paper, and scissor Icon from font-awesome.
-const randomClasses = ["fas fa-hand-rock", "fas fa-hand-paper","fas fa-hand-scissors"];
+
+// Storing the scores.
+let computerScore = 1;
+let playerScore = 1;
+
+const rockIcon = "fas fa-hand-rock";
+const paperIcon = "fas fa-hand-paper";
+const scissorsIcon = "fas fa-hand-scissors";
+
+const randomClasses = [rockIcon, paperIcon, scissorsIcon];
 const text = document.getElementById('demo');
 const text2 = document.getElementById('demo2');
 
-// Game Functionality: Setting forEach function for the buttons.
+const tie = ()=>{
+    text.innerHTML = "It's a Tie ! ";
+    text.style.color = 'orange';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'orange';
+}
+
+const win = ()=>{
+    text.innerHTML = "It's a Win ! ";
+    text.style.color = 'rgb(1, 146, 1)';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'rgb(1, 146, 1)';
+}
+
+const lose = ()=>{
+    text.innerHTML = "You Loosed ! ";
+    text.style.color = 'red';
+    text2.innerHTML = text.innerHTML;
+    text2.style.color = 'red';
+}
+
+// Game Functionality.
 const game = () =>{
     buttons.forEach(btn =>{
         btn.addEventListener('click',(e)=>{
-        // Random rock paper scissor for the computer and the player
+        // Random rock paper scissor for the computer and clicked ones for the player
            let clickedBtn = e.target.className;
            showIcon.className = clickedBtn;
            let randomNum = Math.floor(Math.random() * randomClasses.length);
            computerShowIcon.className = randomClasses[randomNum];
-           // Game Score.
+
            // If it's a Tie .
            if(showIcon.className === computerShowIcon.className){
                pScore.innerHTML = pScore.innerHTML;
                cScore.innerHTML = cScore.innerHTML;
-               text.innerHTML = "It's a Tie ! ";
-               text.style.color = 'orange';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'orange';
-           } 
-          // if it's not a Tie.
-           else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[2]){
+               tie();
+           }
+
+           // if it's not a Tie.
+           else if(showIcon.className === rockIcon && computerShowIcon.className === scissorsIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
-           }else if(showIcon.className === randomClasses[0] && computerShowIcon.className === randomClasses[1]){
+               win();
+           }else if(showIcon.className === rockIcon && computerShowIcon.className === paperIcon){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[2]){
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === scissorsIcon){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[1] && computerShowIcon.className === randomClasses[0]){
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === rockIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
-           }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[0]){
+               win();
+           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === rockIcon){
                cScore.innerHTML = computerScore;
                computerScore++;
-               text.innerHTML = "You Loosed ! ";
-               text.style.color = 'red';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'red';
-           }else if(showIcon.className === randomClasses[2] && computerShowIcon.className === randomClasses[1]){
+               lose();
+           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === paperIcon){
                pScore.innerHTML = playerScore;
                playerScore++;
-               text.innerHTML = "It's a Win ! ";
-               text.style.color = 'rgb(1, 146, 1)';
-               text2.innerHTML = text.innerHTML;
-               text2.style.color = 'rgb(1, 146, 1)';
+               win();
            }
         });
     });
 }
-// Calling the function.
+
+// Run the game.
 game();
-// This function contains all the game logic.
